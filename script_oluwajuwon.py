764f84c2d5365e88b6c46ba2106144d7fca78e7d
oluwajuwon-design <69088172+oluwajuwon-design@users.noreply.github.com>
2020-08-05 20:35:44 +0100

    Create script_oluwajuwon.py
    Python script to print my personal details

---
+++ script_oluwajuwon.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# This script prints my name,email,language,Biostack, and slackusername
+def personal_details():
+    Name = "Kayode_Yetunde"
+    Email = "kayodeoluwajuwon76@gmail.com"
+    Language = "python"
+    Biostack = "Drug_development"
+    Slack_username = "@Oluwajuwon"
+    print("Name:  {}\nEmail: {}\nLanguage: {}\nBiostack: {}\nSlack_username: {}".format(Name, Email,Language,Biostack,Slack_username))
+personal_details()
